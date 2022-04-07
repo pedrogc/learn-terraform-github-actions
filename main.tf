039328a04d9844aa9ec15972a9e0a3ff7af7cd3d
Pedro <pgc@gmail.com>
2022-04-07 11:46:56 +0200

    Point backed to correct workespace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform_Demos_PGC"
 
     workspaces {
       name = "gh-actions-demo"
